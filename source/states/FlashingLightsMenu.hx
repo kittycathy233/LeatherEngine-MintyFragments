29e7fd1cfe5a0eb401488c20219db3da52f255ec
kittycathy233 <2286431462@qq.com>
2025-12-27 19:22:56 +0800

    初次启动小改

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -10,11 +10,17 @@ class FlashingLightsMenu extends MusicBeatState {
 	public var text:FlxText;
 	public var canInput:Bool = true;
 
+	private var whiteFlash:flixel.FlxSprite;
+
 	override public function create() {
 		super.create();
 
-		text = new FlxText(0, 0, 0, 'This game has flashing lights!\nPress Y to enable them, or N to disable them.\n(Either key closes takes you to the title screen.)',
-			32);
+		whiteFlash = new flixel.FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+		whiteFlash.alpha = 0;
+		add(whiteFlash);
+
+		text = new FlxText(0, 0, 0,
+			'This game has flashing lights!\nPress Y to enable them, or N to disable them.\n(Either key closes takes you to the title screen.)', 32);
 		text.font = Paths.font('vcr.ttf');
 		text.screenCenter();
 		text.alignment = CENTER;
@@ -33,14 +39,22 @@ class FlashingLightsMenu extends MusicBeatState {
 
 		if (yes) {
 			Options.setData(true, 'flashingLights');
+			text.text = 'Flashing lights ENABLED!\nNow loading title screen...';
+			text.screenCenter();
+			whiteFlash.alpha = 1;
+			FlxTween.tween(whiteFlash, {alpha: 0}, 1, {
+				ease: FlxEase.quadOut
+			});
 		} else if (no) {
 			Options.setData(false, 'flashingLights');
+			text.text = 'Flashing lights DISABLED.\nNow loading title screen...';
+			text.screenCenter();
 		}
 
 		if (yes || no) {
 			FlxG.sound.play(Paths.sound('confirmMenu'));
 
-			FlxTween.tween(text, {alpha: 0}, 2.0, {
+			FlxTween.tween(text, {alpha: 0}, 3.0, {
 				ease: FlxEase.cubeInOut,
 				onComplete: (_) -> FlxG.switchState(() -> new TitleState())
 			});
